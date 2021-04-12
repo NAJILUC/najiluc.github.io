fa16d7d59913e53b02745c901628c52879bfd75d
Julian Navarro Botello <julianricardonb@ufps.edu.co>
2021-04-12 10:04:35 -0500

    commit 4

--- PrimerPrevio/js/consultas.js
+++ PrimerPrevio/js/consultas.js
@@ -1,3 +1,10 @@
+var myArray;
+
 fetch("http://demo6497253.mockable.io/categoria/deporte")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => (myArray = data));
+
+var foo = myArray.map(function (bar) {
+  return "<li>" + bar.marca + " " + bar.modelo + "</li>";
+});
+document.getElementById("deportes").innerHTML = foo;
